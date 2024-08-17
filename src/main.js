0b37efb8ea3aefc17a2e0684cb50e19f1a24686a
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-08-17 22:30:57 +0000

    modified:   src/index.html 	modified:   src/main.js 	new file:   src/static/cobble.jpg

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 var FPS = 30;
 
 var SQSIZE = 50;
+var WSIZE = 10;
+var HWSIZE = WSIZE / 2;
 
 var plr = {
   pos: new V(100, 100),
@@ -21,11 +23,12 @@ var plr = {
 };
 
 var cam = {
-  pos: new V(0, 0)
+  pos: new V(0, 0),
+  fixed: true,
 }
 
 function start() {
-
+  ge.loadpic('cobble.jpg', 'cobble')
 }
 
 function loop() {
@@ -34,10 +37,10 @@ function loop() {
     ((input.k.w || input.k.arrowdown) ?? 0) - ((input.k.s || input.k.arrowup) ?? 0),
   ).norm().mul(plr.con.speed);
 
-  if(d.mag > 0){
+  if (d.mag > 0) {
     var o = plr.anim.f;
-    plr.anim.f = Math.floor((d.head-45)/90)+2;
-    if(o != plr.anim.f){
+    plr.anim.f = Math.floor((d.head - 45) / 90) + 2;
+    if (o != plr.anim.f) {
       plr.anim.l[plr.anim.f].resetTime();
     }
   }
@@ -45,10 +48,31 @@ function loop() {
   plr.vel.add(d).mul(plr.con.drag);
   plr.pos.add(plr.vel);
 
+  if (cam.fixed)
+    cam.pos = new V(plr.pos);
+  cam.pos.add(HWSIZE * SQSIZE);
+  cam.pos.x = Math.abs(cam.pos.x) > WSIZE * SQSIZE + WIDTH ?
+    Math.sign(cam.pos.x) * WSIZE * SQSIZE + WIDTH : cam.pos.x;
+  cam.pos.y = Math.abs(cam.pos.y) > WSIZE * SQSIZE + HEIGHT ?
+    Math.sign(cam.pos.y) * WSIZE * SQSIZE + HEIGHT : cam.pos.y;
+  cam.pos.sub(HWSIZE * SQSIZE);
+
   mkdraw(plr.pos.y, () => {
     var i = plr.anim.l[plr.anim.f].current;
     __.img(i, [plr.pos.x, plr.pos.y, SQSIZE, SQSIZE]);
-  })
+  });
+
+  var img = ge.getpic('cobble');
+  var h = Math.ceil(HEIGHT/SQSIZE)+1;
+  var w = Math.ceil(WIDTH/SQSIZE)+1;
+  for (var i = 0; i < h; i++) {
+    for (var j = 0; j < w; j++) {
+      var p = new V(i, j).mul(SQSIZE).add(cam.pos);
+      mkdraw(p.y, () => {
+        __.img(img, [p.x, p.y, SQSIZE, SQSIZE]);
+      });
+    }
+  }
 
   draw();
 }
@@ -58,8 +82,9 @@ mkdraw = (z, cb) => todraw.push({ z, cb });
 
 function draw() {
   _.save();
-  _.translate(cam.pos.x, cam.pos.y);
-  todraw.sort((a, b) => b.z - a.z);
+  _.translate(-cam.pos.x, -cam.pos.y);
+  _.translate((WIDTH - SQSIZE) / 2, (HEIGHT - SQSIZE) / 2);
+  todraw.sort((a, b) => a.z - b.z);
   todraw.forEach(x => x.cb());
   todraw = [];
   _.restore();
